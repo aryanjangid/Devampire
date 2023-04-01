a42e52e1e4d0222121e0bc30c28cc7b41e2f8a3e
Pranav Machindra Kale <pranavmachindrakale@EMPEROR-PHEONIXs-MacBook-Air.local>
2023-04-01 15:14:47 +0530

    first commit

---
+++ client/src/Pages/Users/UsersList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import User from "./Users";
+import "./Users.css";
+
+const UsersList = () => {
+  const users = useSelector((state) => state.usersReducer)
+
+  return (
+    <div className="user-list-container">
+      {users.map((user) => (
+        <User user={user} key={user?._id} />
+      ))}
+    </div>
+  );
+};
+
+export default UsersList;
